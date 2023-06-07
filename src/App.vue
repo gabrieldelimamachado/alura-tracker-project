6629034cd51308cad24356e14a866323fb932399
Gabriel de Lima Machado <gabriellimamachado80@gmail.com>
2023-06-07 14:11:51 -0300

    alura-tracker

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo}">
+    <div class="column is-one-quarter">
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
+    </div>
+    <div class="column is-three-quarter conteudo">
+      <FormularioNome @aoSalvarTarefa="salvarTarefa" />
+      <div class="lista">
+
+        <NovaTarefa v-for="(tarefa, index) in  tarefas" :key="index" :tarefa="tarefa" />
+        <NewBox v-if="listaEstaVazia">
+        Você não está muito produtivo hoje :(
+      </NewBox>
+      </div>
+    </div>
+
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BarraLateral from './components/BarraLateral.vue'
+import FormularioNome from './components/FormularioNome.vue'
+import NovaTarefa from './components/NovaTarefa.vue'
+import ITarefa from './interfaces/ITarefa'
+import NewBox from './components/NewBox.vue';
+
+
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BarraLateral,
+    FormularioNome,
+    NovaTarefa,
+    NewBox
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
+    }
+  },
+  computed: {
+    listaEstaVazia () : boolean { 
+      return this.tarefas.length === 0
+
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    },
+    trocarTema (modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo
+
+    }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.lista {
+  padding: 1.25rem;
 }
+
+main {
+  --bg-primario: #ffff;
+  --texto-primario: #000;
+}
+
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+
+}
+
+.conteudo {
+  background-color: var(--bg-primario)
+
+}
+
 </style>
---
+++ src/components/NovaTarefa.vue
@@ -0,0 +1,33 @@
+<template>
+    <NewBox> 
+        <div class="columns">
+            <div class="column is-7">{{tarefa.descricao || 'Tarefa sem descrição'}}</div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+            </div>
+        </div>
+    </NewBox>   
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Cronometro from './Cronometro.vue';
+import ITarefa from '@/interfaces/ITarefa'
+import NewBox from './NewBox.vue'
+
+
+export default defineComponent({
+    name: 'NovaTarefa',
+    components: {
+        Cronometro,
+        NewBox
+    },
+    props: {
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+            required: true    
+        }
+    }
+});        
+</script>
+
